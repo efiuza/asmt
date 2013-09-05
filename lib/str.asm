54d451052715f46ecfbdbd31e6717abd04eb15fe
Emanuel Oliveira <efiuza@me.com>
2013-09-05 15:13:54 -0300

    Decimal converter set to unsigned values + Octal and Hexadecimal values

--- lib/str.asm
+++ lib/str.asm
@@ -11,34 +11,36 @@ global _itoa
 
 section .text
 
-; This function counts characters on null terminated strings.
-; @prototype unsigned int _strlen(const char *)
+; @prototype unsigned _strlen(const char *);
+; String length calculation
 
 _strlen:
 
     ; create new frame
     enter 0, 0
 
-    ; save register bank
+    ; save previous register contents
     push ecx
     push edi
 
-    ; prepare for scan
-    mov edi, [ebp + 8]              ; load first parameter (string pointer)
-    xor ecx, ecx                    ; clear ECX...
-    not ecx                         ; turn it into FFFFFFFFh
-    mov al, 0                       ; mov ascii null byte to AL
-    cld                             ; clear direction flag (inc EDI)
+    ; load registers with parameters
+    mov edi, [ebp + 8]
 
-    ; Scan string at [ES:EDI]. Because DS and ES are assumed to point
-    ; to the same location, there is no need to change memory segments.
-    repne scasb
+    ; main scan loop
+    xor ecx, ecx
+    not ecx
+.scan:
+    mov al, [edi]
+    test al, al
+    jz .return
+    inc edi
+    loop .scan
 
-    ; REPNE decrements ECX after string operation...
-    not ecx                         ; !x = (-1 * x) - 1
-    lea eax, [ecx - 1]              ; load EAX with ECX - 1
+.return:
+    not ecx                         ; ~ecx = (-1 * ecx) - 1
+    mov eax, ecx
 
-    ; restore register bank
+    ; restore previous register contents
     pop edi
     pop ecx
 
@@ -47,117 +49,44 @@ _strlen:
     ret
 
 
-; This function converts a double word integer into a null terminated
-; character string.
-; @prototype void _itoa(int, char *)
 
-_itoa:
+; @prototype char *_utoad(unsigned, char *);
+; Conversion of an unsigned integer into a decimal ascii string
 
-    ; create new frame
-    ; push ebp
-    ; mov ebp, esp
+_utoad:
+
+    ; enter new stack frame
     enter 0, 0
 
-    ; save register bank
+    ; save previous register contents
     push ecx
     push edx
     push ebx
     push esi
     push edi
 
-    ; prepare registers for division loop
-    mov eax, [ebp + 8]              ; load EAX with integer (1st arg)
-    mov esi, [ebp + 12]             ; load ESI with string pointer (2nd arg)
-    mov edi, esi                    ; save a copy of string base
+    ; load registers with paramenters
+    mov eax, [ebp + 8]
+    mov ebx, [ebp + 12]
+    mov edi, ebx
+    mov esi, ebx
+
+    ; main division loop
     mov ecx, 10                     ; load ECX with divisor
-
-    ; check for negative value
-    test eax, eax                   ; check for negative value
-    sets bl                         ; save sign bit in BL
-    jns .divide                     ; skip negative values adjusment
-    neg eax
-
-    ; division loop
-.divide:
+.main_loop:
     cdq                             ; sign extend EAX to EDX:EAX
     div ecx                         ; perform division using ECX as divisor
-    add dl, '0'                     ; add ascii zero code to remainder (EDX)
-    mov [esi], dl                   ; store digit (ESI is an offset of DS)
-    inc esi                         ; adjust pointer to next position
+    add edx, '0'                    ; add ascii zero code to remainder (EDX)
+    mov [edi], dl                   ; store digit (ESI is an offset of DS)
+    inc edi                         ; adjust pointer to next position
     test eax, eax                   ; test quotient
-    jnz .divide                     ; start over while EAX is not zero
-
-    ; set number sign
-    test bl, bl                     ; check for sign
-    jz .terminate
-    mov byte [esi], '-'             ; write sign to string
-    inc esi                         ; adjust pointer to next position
-
-.terminate:
-    mov byte [esi], 0               ; terminate string
-
-    ; reverse string order
-.reverse:
-    dec esi
-    cmp esi, edi
-    jbe .leave                      ; leave if below or equal (unsigned)
-    mov al, [edi]
-    mov ah, [esi]
-    mov [edi], ah
-    mov [esi], al
-    inc edi
-    jmp .reverse
-
-.leave:
-    pop edi
-    pop esi
-    pop ebx
-    pop edx
-    pop ecx
-
-    ; restore precious stack frame
-    ; mov esp, ebp
-    ; pop ebp
-    leave
-    ret
-
-; @prototype char *_utoax(unsigned, char *);
-; Convert unsigned integer into hexadecial ASCII string.
-_utoax:
-    ; enter new stack frame
-    enter 0, 0
-
-    ; save register contents
-    push edx
-    push esi
-    push edi
-
-    ; load parameters
-    mov edx, [ebp + 8]
-    mov esi, [ebp + 12]
-    mov edi, esi
-
-.main_loop:
-    mov eax, edx
-    and eax, 15
-    cmp eax, 10
-    jb .below
-    sub eax, 10
-    add eax, 'a'
-    jmp .continue
-.below:
-    add eax, '0'
-.continue:
-    mov [edi], al
-    inc edi
-    shr edx, 4
-    jnz .main_loop
+    jnz .main_loop                  ; start over while EAX is not zero
 
     ; mark end of string
     mov byte [edi], 0
-    jmp .test_reverse
 
     ; reverse digits order
+    jmp .test_reverse
 .reverse:
     mov al, [esi]
     mov ah, [edi]
@@ -170,11 +99,79 @@ _utoax:
     ja .reverse
 
     ; set return value
-    mov eax, [ebp + 12]
+    mov eax, ebx
 
-    ; restore registers
+    ; restore previous register contents
     pop edi
     pop esi
+    pop ebx
+    pop edx
+    pop ecx
+
+    ; restore previous stack frame
+    leave
+    ret
+
+
+
+; @prototype char *_utoax(unsigned, char *);
+; Conversion of an unsigned integer into an hexadecimal ascii string.
+
+_utoax:
+    ; enter new stack frame
+    enter 0, 0
+
+    ; save previous register contents
+    push edx
+    push ebx
+    push esi
+    push edi
+
+    ; load registers with parameters
+    mov edx, [ebp + 8]
+    mov ebx, [ebp + 12]
+    mov esi, ebx
+    mov edi, ebx
+
+.main_loop:
+    mov eax, edx
+    and eax, 15
+    cmp eax, 10
+    jb .less_than
+    sub eax, 10
+    add eax, 'a'
+    jmp .continue
+.less_than:
+    add eax, '0'
+.continue:
+    mov [edi], al
+    inc edi
+    shr edx, 4
+    jnz .main_loop
+
+    ; mark end of string
+    mov byte [edi], 0
+
+    ; reverse digits order
+    jmp .test_reverse
+.reverse:
+    mov al, [esi]
+    mov ah, [edi]
+    mov [esi], ah
+    mov [edi], al
+    inc esi
+.test_reverse:
+    dec edi
+    cmp edi, esi
+    ja .reverse
+
+    ; set return value
+    mov eax, ebx
+
+    ; restore previous register contents
+    pop edi
+    pop esi
+    pop ebx
     pop edx
 
     ; restore previous stack frame
@@ -182,12 +179,77 @@ _utoax:
     ret
 
 
+
+; @prototype char *_utoao(unsigned, char *);
+; Conversion of an unsigned integer into an octal ascii string.
+
+_utoao:
+
+    ; enter new stack frame
+    enter 0, 0
+
+    ; save previous register contents
+    push edx
+    push ebx
+    push esi
+    push edi
+
+    ; load registers with parameters
+    mov edx, [ebp + 8]
+    mov ebx, [ebp + 12]
+    mov esi, ebx
+    mov edi, ebx
+
+.main_loop:
+    mov eax, edx
+    and eax, 7
+    add eax, '0'
+    mov [edi], al
+    inc edi
+    shr edx, 3
+    jnz .main_loop
+
+    ; mark end of string
+    mov byte [edi], 0
+
+    ; reverse digits order
+    jmp .test_reverse
+.reverse:
+    mov al, [esi]
+    mov ah, [edi]
+    mov [esi], ah
+    mov [edi], al
+    inc esi
+.test_reverse:
+    dec edi
+    cmp edi, esi
+    ja .reverse
+
+    ; set return value
+    mov eax, ebx
+
+    ; restore previous register contents
+    pop edi
+    pop esi
+    pop ebx
+    pop edx
+
+    ; restore previous stack frame
+    leave
+    ret
+
+
+
+; @prototype int _strf(const char *frmt, char *buf, ...);
 ; Apply parameters to a format string
 ; Formats:
 ;     %d or %i => signed decimal integer
+;     %u       => unsigned decimal integer
+;     %o       => unsigned octal integer
+;     %x       => unsigned hexadecimal integer
+;     %X       => unsigned hexadecimal integer (uppercase)
 ;     %c       => character
 ;     %s       => null terminated byte string
-; @prototype int _strf(const char *frmt, char *buf, ...)
 _strf:
 
     enter 4, 0
